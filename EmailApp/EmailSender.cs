cda33a5ef4cee9c13e05cc491b84be8391eee8b2
robert.badea <48626784+ibor99@users.noreply.github.com>
2024-03-19 16:17:21 +0200

    assignment 10 app done (works)

---
+++ EmailApp/EmailSender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailApp
+{
+    public class EmailSender
+    {
+        public EmailSender() { }
+
+        public void SendEmail(string recipientEmail)
+        {
+            string senderEmail = "badearo99@gmail.com";
+            string password = "ytxz ykyy uhsx zlyp";
+
+            MailMessage mail = new MailMessage(senderEmail, recipientEmail);
+            mail.Subject = "Thx for subscribing";
+            mail.Body = "Thank you for subscribing to this ( test )";
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com");
+            client.Port = 587;
+            client.Credentials = new NetworkCredential(senderEmail, password);
+            client.EnableSsl = true;
+
+            try
+            {
+                client.Send(mail);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"Failed to send email : {ex.Message}");
+            }
+            finally
+            {
+                mail.Dispose();
+                client.Dispose();
+            }
+        }
+    }
+}
---
+++ EmailApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmailApp;
+using System;
+using System.Net;
+using System.Net.Mail;
+
+Console.WriteLine("Enter your email address:");
+string recipientEmail = Console.ReadLine();
+
+EmailSender sender = new EmailSender();
+
+sender.SendEmail(recipientEmail);
+
+Console.WriteLine("Email attempted to send. Press any key to exit.");
+Console.ReadKey();
+
